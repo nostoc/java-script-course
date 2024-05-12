530b516086248564470781996bc49909a91c58aa
Hansika <nostochk@gmail.com>
2024-05-12 12:46:45 +0530

    add function info

--- object-constructor.js
+++ object-constructor.js
@@ -3,5 +3,14 @@ function Book(title,author,pages,status){
     this.author=author;
     this.pages=pages;
     this.status=status;
+
+    this.info = function(){
+        console.log(this.title);
+        console.log(this.author);
+        console.log(this.pages);
+        console.log(this.status);
+    }
+
+
 }
 
